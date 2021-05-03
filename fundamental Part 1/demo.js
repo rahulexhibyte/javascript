de6263eaa223f4b75f46827225ef5111e7b42120
rahulexhibyte <rahul.exhibyte@gmail.com>
2021-05-03 18:45:07 +0530

    Fundamental Part 1 : 03/05

---
+++ fundamental Part 1/demo.js
@@ -0,0 +1,70 @@
+// Javascript is a High-Level,Object-Oriented,Multi-paradigm programming language.
+
+// React,Vue & Angular : Frontend with JS
+// Node :Backend with JS
+
+// ES11 - ES2020
+
+let js = "amazing";
+if (js == "amazing") alert("JavaScript is Fun");
+
+console.log(40 + 8 + 25 + 63);
+
+console.log("Jones");
+
+let firstName = "Rahul";
+console.log(firstName);
+
+//Invalid Variable Name
+// let 3years = "3";
+// let y&n = "";
+
+// $ is Valid in Variable Name
+
+let PI = 3.1415;
+
+//DataTypes :
+// Number
+// String
+// Boolean
+
+// undefined : not yet defined to Variable
+// Null
+
+//Symbol
+//BigInt : for Larger Integers
+
+//typeof : for Know type of Variable
+console.log(typeof true);
+console.log(typeof "Hello");
+console.log(typeof 1253);
+console.log(typeof null); //object
+
+let age = 30;
+age = 31;
+
+// const value can't change after initialization
+//immutableValue
+const weight = 65;
+// weight = 67;
+
+//not on scope
+//valid declaration
+//This is not a Good Idea
+notNecessaryValue = "not Necessary";
+console.log(notNecessaryValue);
+
+const now = 2021;
+const ageRahul = now - 2001;
+console.log(ageRahul);
+
+console.log(ageRahul * 2);
+console.log(ageRahul / 2);
+
+// ** use for POwer Function
+console.log(2 ** 3);
+
+const firstName = "Rahul";
+const lastName = "Rathod";
+
+console.log(firstName + " " + lastName);
